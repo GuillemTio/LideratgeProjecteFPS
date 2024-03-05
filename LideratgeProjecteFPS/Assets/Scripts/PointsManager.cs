0b1429ad9ff534a17118a7364b9146a38f94d143
GuillemTio <gtio@edu.tecnocampus.cat>
2024-03-05 19:54:22 +0100

    points manager

--- LideratgeProjecteFPS/Assets/Scripts/PointsManager.cs
+++ LideratgeProjecteFPS/Assets/Scripts/PointsManager.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PointsManager : MonoBehaviour
 {
     [SerializeField] float pointsPerKill = 100f;
-    [SerializeField] float killStreakMultiplier = 1.25f;
+    [SerializeField] float addToMultiplierForEachKill = 0.25f;
+    [SerializeField] float multiplierStart = 1f;
     [SerializeField] float killStreakTime = 2f;
+    float killStreakMultiplier;
     private bool isOnKillStreak = false;
     private float killTimer;
     private float currentPoints;
@@ -14,17 +14,29 @@ public class PointsManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        killStreakMultiplier = multiplierStart;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isOnKillStreak)
+        {
+            killTimer += Time.deltaTime;
+            if (killTimer >= killStreakTime)
+            {
+                killTimer = 0;
+                isOnKillStreak = false;
+                killStreakMultiplier = multiplierStart;
+            }
+        }
     }
 
     public void EnemyKilled()
     {
-
+        isOnKillStreak = true;
+        killTimer = 0;
+        currentPoints += pointsPerKill * killStreakMultiplier;
+        killStreakMultiplier += addToMultiplierForEachKill;
     }
 }
--- LideratgeProjecteFPS/Assets/_Scripts/SimpleEnemy.cs
+++ LideratgeProjecteFPS/Assets/_Scripts/SimpleEnemy.cs
@@ -77,6 +77,8 @@ class SimpleEnemy : MonoBehaviour, IShootable
     private void Die()
     {
         gameObject.SetActive(false);
+
+        GameObject.FindGameObjectWithTag("PointsManager").GetComponent<PointsManager>().EnemyKilled();
         // de momento lo desactivo, si nos interesa destruirlo o pasarlo por un game manager ya lo vemos.
     }
 
